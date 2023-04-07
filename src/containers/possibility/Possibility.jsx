5c49a3b650457ec1915d4cf1cd281f63618449a5
Jamester Go <jamester.c.go@accenture.com>
2023-04-07 17:32:40 +0800

    initial code and folder creation

---
+++ src/containers/possibility/Possibility.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './possibility.css';
+
+const Possibility = () => {
+  return (
+    <div>
+      <div>Possibility</div>
+    </div>
+  )
+}
+
+export default Possibility
---
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './WhatGPT3';
+
+const WhatGPT3 = () => {
+  return (
+    <div>
+      <div>What GPT3</div>
+    </div>
+  )
+}
+
+export default WhatGPT3
